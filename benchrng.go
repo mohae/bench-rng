9aa6e0262c2c8a166a664ff3951ccfc99329aec0
Joel Scoble <joel.scoble@outlook.com>
2016-05-07 23:40:15 -0500

    refactor into a cli app; use benchutil to generate output

---
+++ benchrng.go
@@ -0,0 +1,157 @@
+package main
+
+import (
+	c "crypto/rand"
+	"math/big"
+	m "math/rand"
+	"testing"
+
+	mt64e "github.com/EricLagergren/go-prng/mersenne_twister_64"
+	xorshift "github.com/EricLagergren/go-prng/xorshift"
+	"github.com/MichaelTJones/pcg"
+	mt64b "github.com/bszcz/mt19937_64"
+	pcgr "github.com/dgryski/go-pcgr"
+	"github.com/mohae/benchutil"
+	mt64s "github.com/seehuhn/mt19937"
+)
+
+func CryptoRand(b *testing.B) {
+	b.StopTimer()
+	bi := big.NewInt(maxInt64)
+	var n *big.Int
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		n, _ = c.Int(c.Reader, bi)
+	}
+	_ = n
+}
+
+func BenchCryptoRand() benchutil.Bench {
+	bench := benchutil.Bench{Name: "crypto/rand"}
+	bench.Result = benchutil.ResultFromBenchmarkResult(testing.Benchmark(CryptoRand))
+	return bench
+}
+
+func MathRand(b *testing.B) {
+	b.StopTimer()
+	m.Seed(benchutil.SeedVal())
+	var n int64
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		n = m.Int63()
+	}
+	_ = n
+}
+
+func BenchMathRand() benchutil.Bench {
+	bench := benchutil.Bench{Name: "math/rand"}
+	bench.Result = benchutil.ResultFromBenchmarkResult(testing.Benchmark(MathRand))
+	return bench
+}
+
+func DgryskiGoPCGR(b *testing.B) {
+	b.StopTimer()
+	var n int64
+	var rnd pcgr.Rand
+	rnd.Seed(benchutil.SeedVal())
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		n = rnd.Int63()
+	}
+	_ = n
+}
+
+func BenchDgryskiGoPCGR() benchutil.Bench {
+	bench := benchutil.Bench{Name: "dgryski/go-pcgr"}
+	bench.Result = benchutil.ResultFromBenchmarkResult(testing.Benchmark(DgryskiGoPCGR))
+	return bench
+}
+
+func MichaelTJonesPCG(b *testing.B) {
+	b.StopTimer()
+	var n uint64
+	rnd := pcg.NewPCG64()
+	rnd.Seed(uint64(benchutil.SeedVal()), uint64(benchutil.SeedVal()), uint64(benchutil.SeedVal()), uint64(benchutil.SeedVal()))
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		n = rnd.Random()
+	}
+	_ = n
+}
+
+func BenchMichaelTJonesPCG() benchutil.Bench {
+	bench := benchutil.Bench{Name: "MichaelTJones/pcg"}
+	bench.Result = benchutil.ResultFromBenchmarkResult(testing.Benchmark(MichaelTJonesPCG))
+	return bench
+}
+
+func BszczMT64(b *testing.B) {
+	b.StopTimer()
+	var n int64
+	rnd := mt64b.New()
+	rnd.Seed(benchutil.SeedVal())
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		n = rnd.Int63()
+	}
+	_ = n
+}
+
+func BenchBszczMT64() benchutil.Bench {
+	bench := benchutil.Bench{Name: "bszcz/mt19937_64"}
+	bench.Result = benchutil.ResultFromBenchmarkResult(testing.Benchmark(BszczMT64))
+	return bench
+}
+
+func EricLagergrenMT64(b *testing.B) {
+	b.StopTimer()
+	var n int64
+	rnd := mt64e.NewMersenne(benchutil.SeedVal())
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		n = rnd.Int63()
+	}
+	_ = n
+}
+
+func BenchEricLagergrenMT64() benchutil.Bench {
+	bench := benchutil.Bench{Name: "EricLagergren/go-prng/mersenne_twister_64"}
+	bench.Result = benchutil.ResultFromBenchmarkResult(testing.Benchmark(EricLagergrenMT64))
+	return bench
+}
+
+func SeehuhnMT64(b *testing.B) {
+	b.StopTimer()
+	var n int64
+	rnd := mt64s.New()
+	rnd.Seed(benchutil.SeedVal())
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		n = rnd.Int63()
+	}
+	_ = n
+}
+
+func BenchSeehuhnMT64() benchutil.Bench {
+	bench := benchutil.Bench{Name: "seehuhn/mt19937"}
+	bench.Result = benchutil.ResultFromBenchmarkResult(testing.Benchmark(SeehuhnMT64))
+	return bench
+}
+
+func EricLagergrenXORShift(b *testing.B) {
+	b.StopTimer()
+	var n uint64
+	var rnd xorshift.Shift1024Star
+	rnd.Seed()
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		n = rnd.Next()
+	}
+	_ = n
+}
+
+func BenchEricLagergrenXORShift() benchutil.Bench {
+	bench := benchutil.Bench{Name: "EricLagergren/go-prng/xorshift"}
+	bench.Result = benchutil.ResultFromBenchmarkResult(testing.Benchmark(EricLagergrenXORShift))
+	return bench
+}
---
+++ main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/mohae/benchutil"
+)
+
+const maxInt64 = 1<<63 - 1
+
+// flags
+var (
+	output string
+	format string
+)
+
+func init() {
+	flag.StringVar(&output, "output", "stdout", "output destination")
+	flag.StringVar(&output, "o", "stdout", "output destination (short)")
+	flag.StringVar(&format, "format", "txt", "format of output")
+	flag.StringVar(&format, "f", "txt", "format of output")
+}
+
+func main() {
+	flag.Parse()
+	done := make(chan struct{})
+	go benchutil.Dot(done)
+
+	// set the output
+	var w io.Writer
+	var err error
+	switch output {
+	case "stdout":
+		w = os.Stdout
+	default:
+		w, err = os.OpenFile(output, os.O_CREATE|os.O_TRUNC|os.O_RDWR, 0644)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		defer w.(*os.File).Close()
+	}
+	// get the benchmark for the desired format
+	// process the output
+	var bench benchutil.Benchmarker
+	switch format {
+	case "csv":
+		bench = benchutil.NewCSVBench(w)
+	case "md":
+		bench = benchutil.NewMDBench(w)
+	default:
+		bench = benchutil.NewStringBench(w)
+	}
+
+	b := BenchMathRand()
+	bench.Add(b)
+
+	b = BenchCryptoRand()
+	bench.Add(b)
+
+	b = BenchDgryskiGoPCGR()
+	bench.Add(b)
+
+	b = BenchMichaelTJonesPCG()
+	bench.Add(b)
+
+	b = BenchBszczMT64()
+	bench.Add(b)
+
+	b = BenchEricLagergrenMT64()
+	bench.Add(b)
+
+	b = BenchSeehuhnMT64()
+	bench.Add(b)
+
+	b = BenchEricLagergrenXORShift()
+	bench.Add(b)
+
+	fmt.Println("")
+	fmt.Println("generating output...")
+	err = bench.Out()
+	if err != nil {
+		fmt.Printf("error generating output: %s\n", err)
+	}
+}
